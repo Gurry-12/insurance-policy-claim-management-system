27255fdcd9d0b3cf3f601ff366ec28c746f8b406
Gurry-12 <96852785+Gurry-12@users.noreply.github.com>
2026-06-07 18:14:42 +0530

    feat: refector the authentication method

--- src/main/java/com/insurance/demo/controller/AuthController.java
+++ src/main/java/com/insurance/demo/controller/AuthController.java
@@ -1,35 +1,47 @@
 package com.insurance.demo.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.insurance.demo.dto.request.LoginRequestDTO;
 import com.insurance.demo.dto.request.UserRequestDTO;
 import com.insurance.demo.dto.response.ApiResponseDTO;
+import com.insurance.demo.dto.response.LoginResponseDTO;
 import com.insurance.demo.dto.response.UserResponseDTO;
-import com.insurance.demo.serviceImpl.AuthServiceImpl;
+import com.insurance.demo.repository.AppUserRepository;
+import com.insurance.demo.security.JwtService;
+import com.insurance.demo.service.AuthService;
+import com.insurance.demo.service.UserService;
 
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/auth")
 @AllArgsConstructor
 public class AuthController {
 
-	@Autowired
-	private AuthServiceImpl authService;
+	private final AuthService authService;
 
 	@PostMapping("/login")
-	public ResponseEntity<String> login(@RequestParam String username, @RequestParam String password) {
+	public LoginResponseDTO login(@Valid @RequestBody LoginRequestDTO requestDto) {
 
-		return ResponseEntity.ok(authService.verify(username, password));
+		log.info("Login request received for email: {}", requestDto.getEmail());
+
+		return authService.login(requestDto);
+
 	}
 
 	@PostMapping("/register")
