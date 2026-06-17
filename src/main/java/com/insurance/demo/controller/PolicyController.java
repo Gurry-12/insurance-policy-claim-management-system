1af66dc7a9d02fc5e8afe2ef7a0e9cc9feba9ef9
Gurry-12 <96852785+Gurry-12@users.noreply.github.com>
2026-06-17 10:19:51 +0530

    feat: implement the swagger

--- src/main/java/com/insurance/demo/controller/PolicyController.java
+++ src/main/java/com/insurance/demo/controller/PolicyController.java
@@ -20,7 +20,9 @@ import com.insurance.demo.dto.response.PageResponseDTO;
 import com.insurance.demo.dto.response.PolicyResponseDTO;
 import com.insurance.demo.service.PolicyService;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
@@ -28,6 +30,7 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/policies")
 @RequiredArgsConstructor
 @SecurityRequirement(name = "bearerAuth")
+@Tag(name = "6. Insurance Policy API", description = "Endpoints for purchasing, issuing, and managing customer policies")
 public class PolicyController {
 
 	private final PolicyService policyService;
@@ -35,6 +38,7 @@ public class PolicyController {
 	@PostMapping("/purchase")
 	@ResponseStatus(HttpStatus.CREATED)
 	@PreAuthorize("hasRole('CUSTOMER')")
+	@Operation(summary = "Purchase Policy", description = "Allows a customer to purchase an active policy plan. The policy will be created in PENDING_PAYMENT status.")
 	public ApiResponseDTO<PolicyResponseDTO> purchasePolicy(@Valid @RequestBody PolicyPurchaseRequestDTO requestDTO) {
 
 		return policyService.purchasePolicy(requestDTO);
@@ -43,6 +47,7 @@ public class PolicyController {
 	@PostMapping("/issue")
 	@PreAuthorize("hasAnyRole('ADMIN', 'AGENT')")
 	@ResponseStatus(HttpStatus.CREATED)
+	@Operation(summary = "Issue Policy", description = "Manually issues a policy to a customer. Restricted to Admin/Agent.")
 	public ApiResponseDTO<PolicyResponseDTO> issuePolicy(@Valid @RequestBody PolicyIssueRequestDTO requestDTO) {
 
 		return policyService.issuePolicy(requestDTO);
@@ -50,6 +55,7 @@ public class PolicyController {
 
 	@GetMapping("/my-policies")
 	@PreAuthorize("hasRole('CUSTOMER')")
+	@Operation(summary = "Get Policy of loged in customer", description = "Retrieves the details of a loged in customer's purchased policy.")
 	public PageResponseDTO<PolicyResponseDTO> getMyPolicies(Authentication authentication,
 			@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size,
 			@RequestParam(defaultValue = "id") String sortBy, @RequestParam(defaultValue = "asc") String direction) {
@@ -59,6 +65,7 @@ public class PolicyController {
 
 	@GetMapping("/customer/{customerId}")
 	@PreAuthorize("hasAnyRole('ADMIN', 'AGENT')")
+	@Operation(summary = "Get Policy by customer ID", description = "Retrieves the details of a specific customer's purchased policy.")
 	public PageResponseDTO<PolicyResponseDTO> getPoliciesByCustomer(@PathVariable Long customerId,
 			@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size,
 			@RequestParam(defaultValue = "id") String sortBy, @RequestParam(defaultValue = "asc") String direction) {
@@ -68,6 +75,7 @@ public class PolicyController {
 
 	@GetMapping
 	@PreAuthorize("hasAnyRole('ADMIN', 'AGENT')")
+	@Operation(summary = "Get All Policies", description = "Retrieves a paginated list of all policies. Supports filtering by customer ID, plan ID, and status.")
 	public PageResponseDTO<PolicyResponseDTO> getAllPolicies(@RequestParam(defaultValue = "0") int page,
 			@RequestParam(defaultValue = "10") int size, @RequestParam(defaultValue = "id") String sortBy,
 			@RequestParam(defaultValue = "asc") String direction,
@@ -79,12 +87,14 @@ public class PolicyController {
 
 	@GetMapping("/{policyId}")
 	@PreAuthorize("hasAnyRole('ADMIN', 'AGENT', 'CUSTOMER')")
+	@Operation(summary = "Get Policy by ID", description = "Retrieves the details of a specific purchased policy.")
 	public ApiResponseDTO<PolicyResponseDTO> getPolicyById(@PathVariable Long policyId) {
 		return policyService.getPolicyById(policyId);
 	}
 	
 	@PatchMapping("/{policyId}/cancel")
 	@PreAuthorize("hasAnyRole('ADMIN', 'AGENT')")
+	@Operation(summary = "Cancel Policy", description = "Cancels an active or pending insurance policy.")
 	public ApiResponseDTO<PolicyResponseDTO> cancelPolicy(@PathVariable Long policyId) {
 
 		return policyService.cancelPolicy(policyId);
